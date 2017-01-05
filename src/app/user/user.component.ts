1639fdca33d223c429f1768722626370763dbee4
Adam Ehven <aehven@binarytrees.biz>
2017-01-05 14:30:02 -0700

    cleanup; get user 1 from user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Angular2TokenService } from 'angular2-token';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,9 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tokenService: Angular2TokenService, private userService: UserService) {}
 
   ngOnInit() {
+    this.userService.show(1);
+    // this.userService.index();
+    // this.userService.create({email: "x18@null.com", first_name: "x0", password: "password", role: "admin"});
+    // this.userService.update(1, {first_name: 'blah14'});
   }
 
 }

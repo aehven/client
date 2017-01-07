7ed651ec8dcd877f985b5b7aaf2490b7005dad0e
Adam Ehven <aehven@binarytrees.biz>
2017-01-06 17:55:01 -0700

    user form stuff

--- src/app/shared/my-validators.ts
+++ src/app/shared/my-validators.ts
@@ -1,23 +1,15 @@
-// import { Validators } from '@angular/forms';
-
 export class MyValidators {
-
-    static mailFormat(control): ValidationResult {
-        if(control.value == null) {
-          return null;
-        }
-        
-        var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-
-        if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
-            return { "incorrectMailFormat": true };
-        }
-
+  static mailFormat(control) {
+      if(control.value == null) {
         return null;
-    }
+      }
 
-}
+      var EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
 
-interface ValidationResult {
-    [key: string]: boolean;
+      if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
+          return { "mailFormat": true };
+      }
+
+      return null;
+  }
 }

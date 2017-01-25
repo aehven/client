c603094bd91b895d4bc16b6c5feba2bd6e67956f
Adam Ehven <aehven@binarytrees.biz>
2017-01-25 09:12:27 -0700

    production uses demo password

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiPath: "http://ror-ng-starter.herokuapp.com/",
-  demoUser: "",
-  demoPassword: ""
+  demoUser: "a0@null.com",
+  demoPassword: "password"
 };

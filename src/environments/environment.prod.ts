e46497e6bd197b65a90200de00b28585bc39a75b
Adam Ehven <aehven@binarytrees.biz>
2017-02-06 09:25:19 -0700

    tweak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiPath: "https://ror-ng-starter.herokuapp.com/",
+  apiPath: "https://ror-ng-starter-production.herokuapp.com/",
   demoUser: "a0@null.com",
   demoPassword: "password"
 };
